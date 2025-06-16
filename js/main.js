70f8a7330987438b0e591c1abd1b001be2e9a9e7
Juancariva <juancarivarola0@gmail.com>
2025-06-16 15:34:51 -0300

    Creación del juego de Adivinar el número secreto

--- js/main.js
+++ js/main.js
@@ -1,4 +1,38 @@
-let nombre = "Juan";
-let edad = 30;
+alert("¡Bienvenido al juego de adivinar el número!");
 
-console.log(nombre);
+let puntaje = 0;
+let nivelesSuperados = [];
+
+function jugarNivel(nivel) {
+  let numeroSecreto = Math.floor(Math.random() * (nivel * 5)) + 1;
+  let intentos = 3;
+  let adivinanza;
+
+  while (intentos > 0) {
+    adivinanza = parseInt(prompt(`Nivel ${nivel} - Adivina el número (1 a ${nivel * 5}). Te quedan ${intentos} intentos:`));
+
+    if (adivinanza === numeroSecreto) {
+      alert("¡Correcto!");
+      puntaje = puntaje + (nivel * 10);
+      nivelesSuperados.push(nivel);
+      return true;
+    } else if (adivinanza > numeroSecreto) {
+      alert("Demasiado alto");
+    } else {
+      alert("Demasiado bajo");
+    }
+    intentos--;
+  }
+
+  alert(`Perdiste. El número era ${numeroSecreto}`);
+  return false;
+}
+
+let nivel = 1;
+while (true) {
+  let seguir = jugarNivel(nivel);
+  if (!seguir) break;
+  nivel++;
+}
+
+alert(`Juego terminado. Puntaje: ${puntaje}\nNiveles superados: ${nivelesSuperados.join(", ")}`);
